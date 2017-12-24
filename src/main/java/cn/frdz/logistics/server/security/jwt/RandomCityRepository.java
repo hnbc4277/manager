a19158a65842ee2e1ac6f35a322ef5329b5c6524
hnbc4277 <304930518@qq.com>
2017-12-24 14:34:13 +0800

    增加权限实体 删除测试文件

--- src/main/java/cn/frdz/logistics/server/security/jwt/RandomCityRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.frdz.logistics.server.security.jwt;
-
-import org.springframework.data.repository.CrudRepository;
-
-/**
- * Created by nydiarra on 10/05/17.
- */
-public interface RandomCityRepository extends CrudRepository<RandomCity, Long> {
-}
--- src/main/java/cn/frdz/logistics/server/security/jwt/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.frdz.logistics.server.security.jwt;
-
-import org.springframework.data.repository.CrudRepository;
-
-/**
- * Created by nydiarra on 06/05/17.
- */
-public interface RoleRepository extends CrudRepository<Role, Long> {
-}
